8ebd98b15d32f5f7480581b0fcc753bba517b83b
dumbus <dumbusx@gmail.com>
2023-11-12 23:23:32 +0300

    feat: add sniffing of syn packets on server

--- client.py
+++ client.py
@@ -1,12 +1,12 @@
-import socket
 import random
 import sys
 
-from scapy.all import IP, TCP, sr1, send
+from scapy.all import conf, get_if_addr, IP, TCP, sr, send
 
-SERVER_IP = "192.168.56.1"
+# SERVER_IP = "192.168.1.102" # prod version
+SERVER_IP = get_if_addr(conf.iface) # dev version
 SERVER_PORT = 5000
-CLIENT_IP = socket.gethostbyname(socket.gethostname())
+CLIENT_IP = get_if_addr(conf.iface)
 CLIENT_PORT = random.randint(1024, 65535)
 
 def client_main():
@@ -17,10 +17,10 @@ def client_main():
 def connect_to_server():
     # Send SYN to a listening server
     ip = IP(src=CLIENT_IP, dst=SERVER_IP)
-    syn = TCP(sport=CLIENT_PORT, dport=SERVER_PORT, flags="S", seq=100)
+    syn = TCP(sport=CLIENT_PORT, dport=SERVER_PORT, flags="S", seq=0)
 
     # Listen for the server's response (SYN/ACK)
-    synack = sr1(ip/syn, timeout=2)
+    synack = sr(ip/syn, timeout=2)
 
     if (synack == None or synack[TCP].flags != 18):
         print("[ERROR] No connection with TCP server.")
--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 import socket
-from scapy.all import sniff
+from scapy.all import *
 
-SERVER_IP = socket.gethostbyname(socket.gethostname())
+SERVER_IP = get_if_addr(conf.iface)
 SERVER_PORT = 5000
+# INTERFACE = ""
+INTERFACE = "\\Device\\NPF_Loopback" # for local testing
 
 def server_main():
     print(f"[STARTED] Server started.")
@@ -18,13 +20,30 @@ def listen_for_client():
 
     server_socket.accept()
 
-# def custom_listen_to_client():
-#     listening = True
+def custom_listen_to_client():
+    print(f"[LISTENING] Server is listening at: {SERVER_IP}:{SERVER_PORT}.")
 
-#     while listening:
-#         # sniff(filter = f"tcp and port {SERVER_PORT}")
-#         sniff(filter = 'tcp', prn=lambda x: x.summary())
+    listening = True
 
+    while listening:
+        # sniff(filter = f"tcp and port {SERVER_PORT}", prn=lambda x: x.summary())
+        sniff(filter = f"tcp and port {SERVER_PORT}", prn=handle_connection, iface=INTERFACE) # for local testing
+
+def handle_connection(packet):
+    if (packet[TCP].flags == "S"):
+        src = SERVER_IP
+        dst = packet[IP].src
+        ip = IP(src=src, dst=dst)
+        
+        sport=SERVER_PORT
+        dport = packet[TCP].sport
+        seg_len = len(packet[TCP].payload)
+        seq = packet[TCP].seq
+        ack = seq + seg_len
+        synack = TCP(sport=sport, dport=dport, flags="SA", seq=seq, ack=ack)
+
+        send(ip/synack)
+    
 
 if __name__ == '__main__':
     server_main()
