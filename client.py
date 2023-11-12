014e0d050625480d5a3accd081dc8cca2b089949
dumbus <dumbusx@gmail.com>
2023-11-12 19:48:25 +0300

    feat: add connection to server at client

--- client.py
+++ client.py
@@ -1,13 +1,29 @@
 import socket
 import random
 
+from scapy.all import *
+
 SERVER_IP = "192.168.56.1"
 SERVER_PORT = 5000
 CLIENT_IP = socket.gethostbyname(socket.gethostname())
 CLIENT_PORT = random.randint(1024, 65535)
 
 def client_main():
-    print("Hello from client")
+    print(f"[STARTED] Client {CLIENT_IP}:{CLIENT_PORT} started.")
+    connect_to_server()
+
+
+def connect_to_server():
+    # Send SYN to a listening server
+    ip = IP(src=CLIENT_IP, dst=SERVER_IP)
+    syn = TCP(sport=CLIENT_PORT, dport=SERVER_PORT, flags="S", seq=100)
+
+    # Listen for the server's response (SYN/ACK)
+    synack = sr1(ip/syn)
+
+    # Send an acknowledgement from client for server's response (ACK)
+    ack = TCP(sport=CLIENT_PORT, dport=SERVER_PORT, flags="A", seq=synack.ack, ack=synack.seq + 1)
+    send(ip/ack)
 
 if __name__ == '__main__':
     client_main()
--- server.py
+++ server.py
@@ -4,7 +4,19 @@ SERVER_IP = socket.gethostbyname(socket.gethostname())
 SERVER_PORT = 5000
 
 def server_main():
-    print("Hello from server")
+    print(f"[STARTED] Server started.")
+    listen_for_client()
+
+def listen_for_client():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((SERVER_IP, SERVER_PORT))
+    server_socket.listen(1)
+
+    print(f"[LISTENING] Server is listening at: {SERVER_IP}:{SERVER_PORT}.")
+
+    while True:
+        client, address = server_socket.accept()
+        
 
 if __name__ == '__main__':
     server_main()
