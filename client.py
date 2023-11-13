6c37d1d65fce38510bdaffa56f09aa76e9f9a8cd
dumbus <dumbusx@gmail.com>
2023-11-13 20:48:40 +0300

    refactor: rename functions and add function to generate fake ip addresses

--- client.py
+++ client.py
@@ -10,45 +10,50 @@ CLIENT_PORT = random.randint(1024, 65535)
 # INTERFACE = "" # prod version
 INTERFACE = "\\Device\\NPF_Loopback" # for local testing
 
-# TODO: create function to generate correct random IPs
-random_ip_addresses = ["210.181.2.14", "200.3.48.237", "230.38.116.75", "177.241.188.245", "136.119.151.17", "154.177.246.190", "252.213.126.122", "85.106.73.28", "115.40.74.18", "236.53.173.79", "109.15.97.23", "6.82.78.24", "6.1.226.159", "204.24.207.127", "44.181.52.192", "31.201.82.101", "229.65.126.232", "137.204.211.126", "175.33.19.207", "210.107.161.191"]
-
 def client_main():
     print(f"[STARTED] Client {CLIENT_IP}:{CLIENT_PORT} started.")
     connect_to_server()
 
 def connect_to_server():
     # Send SYN to a listening server
-    ip = get_spoofed_ip_layer()
-    raw = get_raw_data_with_ip()
+    ip = get_custom_ip_layer()
+    raw = get_custom_data_layer()
     syn = TCP(sport=CLIENT_PORT, dport=SERVER_PORT, flags="S", seq=0)
-    send(ip/syn/raw)
+    send(ip/syn/raw, verbose=0)
 
     # Listen for the server's response (SYN/ACK)
     synack = sniff(filter = f"tcp and port {SERVER_PORT}", iface=INTERFACE, count=1)[0] # for local testing
     #synack =  sniff(filter = f"tcp and port {SERVER_PORT}", count=1)[0] # prod version
 
-    if (synack == None or synack[TCP].flags != 18):
+    if (synack == None or synack[TCP].flags != "SA"):
         print("[ERROR] No connection with TCP server.")
         sys.exit()
     else:
         # Send an acknowledgement from client for server's response (ACK)
         ack = TCP(sport=CLIENT_PORT, dport=SERVER_PORT, flags="A", seq=synack.ack, ack=synack.seq + 1)
-        send(ip/ack/raw)
+        send(ip/ack/raw, verbose=0)
 
-def get_spoofed_ip_layer(dst=SERVER_IP):
-    index = random.randint(0, len(random_ip_addresses) - 1)
-    spoofed_ip_address = random_ip_addresses[index]
+def get_custom_ip_layer(dst=SERVER_IP):
+    ip_parts = []
 
-    spoofed_ip_layer = IP(src=spoofed_ip_address, dst=dst)
+    for i in range(4):
+        if (i != 0):
+            ip_part = random.randint(0, 255)
+        else:
+            ip_part = random.randint(1, 255)
+        
+        ip_parts.append(str(ip_part))
 
-    return spoofed_ip_layer
+    spoofed_ip_address = '.'.join(ip_parts)
+    custom_ip_layer = IP(src=spoofed_ip_address, dst=dst)
 
-def get_raw_data_with_ip(data=""):
+    return custom_ip_layer
+
+def get_custom_data_layer(data=""):
     data_with_ip = f"{CLIENT_IP}__{data}"
-    raw_data_with_ip = Raw(data_with_ip)
+    custom_data_layer = Raw(data_with_ip)
 
-    return raw_data_with_ip
+    return custom_data_layer
 
 if __name__ == '__main__':
     client_main()
--- server.py
+++ server.py
@@ -6,14 +6,11 @@ SERVER_PORT = 5000
 # INTERFACE = "" # prod version
 INTERFACE = "\\Device\\NPF_Loopback" # for local testing
 
-# TODO: create function to generate correct random IPs
-random_ip_addresses = ["210.181.2.14", "200.3.48.237", "230.38.116.75", "177.241.188.245", "136.119.151.17", "154.177.246.190", "252.213.126.122", "85.106.73.28", "115.40.74.18", "236.53.173.79", "109.15.97.23", "6.82.78.24", "6.1.226.159", "204.24.207.127", "44.181.52.192", "31.201.82.101", "229.65.126.232", "137.204.211.126", "175.33.19.207", "210.107.161.191"]
-
 def server_main():
     print(f"[STARTED] Server started.")
-    custom_listen_to_client()
+    listen_for_connection()
 
-def custom_listen_to_client():
+def listen_for_connection():
     print(f"[LISTENING] Server is listening at: {SERVER_IP}:{SERVER_PORT}.")
 
     listening = True
@@ -25,24 +22,32 @@ def custom_listen_to_client():
 def handle_connection(packet):
     if (packet[TCP].flags == "S"):
         dst = get_ip_from_payload(packet)
-        ip = get_spoofed_ip_layer(dst)
+        ip = get_custom_ip_layer(dst)
         
         sport=SERVER_PORT
         dport = packet[TCP].sport
         seg_len = len(packet[TCP].payload)
         seq = packet[TCP].seq
         ack = seq + seg_len
-        synack = TCP(sport=sport, dport=dport, flags="SA", seq=seq, ack=ack)
 
+        synack = TCP(sport=sport, dport=dport, flags="SA", seq=seq, ack=ack)
         send(ip/synack, verbose=0)
 
-def get_spoofed_ip_layer(dst):
-    index = random.randint(0, len(random_ip_addresses) - 1)
-    spoofed_ip_address = random_ip_addresses[index]
+def get_custom_ip_layer(dst):
+    ip_parts = []
 
-    spoofed_ip_layer = IP(src=spoofed_ip_address, dst=dst)
+    for i in range(4):
+        if (i != 0):
+            ip_part = random.randint(0, 255)
+        else:
+            ip_part = random.randint(1, 255)
+        
+        ip_parts.append(str(ip_part))
 
-    return spoofed_ip_layer
+    spoofed_ip_address = '.'.join(ip_parts)
+    custom_ip_layer = IP(src=spoofed_ip_address, dst=dst)
+
+    return custom_ip_layer
 
 def get_ip_from_payload(packet):
     raw_text_data = bytes(packet[TCP].payload).decode('UTF8','replace')
